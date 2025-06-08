d44ffe0f0d5fc1cece9e6e27391335182d893b4a
Matheus Buniotto <matheus.buniotto@gmail.com>
2025-06-08 19:11:18 -0300

    Feat: Adiciona função para tratar strings

---
+++ app/utils/strings_clean_utils.py
@@ -0,0 +1,18 @@
+def clean_vaga_areas_atuacao(df, column_name='vaga_areas_atuacao'):
+    """
+    Limpa as strings de uma coluna específica de um DataFrame,
+    substituindo espaços e hífens por underlines, removendo espaços extras
+    Args:
+        df (pd.DataFrame): The input DataFrame.
+        column_name (str): The name of the column to clean. Defaults to 'vaga_areas_atuacao'.
+
+    Returns:
+        pd.Series: The cleaned column.
+    """
+    cleaned_column = (
+        df[column_name]
+        .str.replace(r'[-\s]+', '_', regex=True)
+        .str.strip('_')
+        .str.strip()
+    )
+    return cleaned_column
--- todo.md
+++ todo.md
@@ -1,8 +1,9 @@
 # TODO
 
-[X] Explorar area de atuação e alguma outra coluna técnica com embeding
+[X] Explorar area de atuação e alguma outra coluna técnica com embedding
 [X] Definir variaveis categóricas e quais são ordinais
 [X] Entender valores nulos remover
+[ ] Usar coluna de area de atuação como categorica e não embedding
 [ ] Entender como fazer funcionar na API recebendo uma nova vaga. (retrieval + rank)
 [ ] API
 
